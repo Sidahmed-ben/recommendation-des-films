e12269a82ac46afd9600966a6446248acf0e590d
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2023-11-04 18:03:33 +0100

    add login and registration

--- Front/movie-recommendation/src/App.jsx
+++ Front/movie-recommendation/src/App.jsx
@@ -1,25 +1,38 @@
 import * as React from 'react'
 import ResponsiveDrawer from './components/Drawer'
-import SearchBar from './components/Search'
 import Grid from '@mui/material/Grid'
-import Movies from './components/Movie/Movies'
-import { MovieProvider } from './MovieData'
+import Home from './pages/HomePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RecommendMoviesPage from './pages/RecommendMoviesPage'
 
 function HomePage () {
+    let isPhone = false
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) {
+        isPhone = true
+    }
+
     return (
-        <div className="App">
-            <Grid container spacing={0}>
-                <Grid item xs={2}>
-                    <ResponsiveDrawer></ResponsiveDrawer>
-                </Grid>
-                <Grid item xs={9}>
-                    <MovieProvider>
-                        <SearchBar></SearchBar>
-                        <Movies></Movies>
-                    </MovieProvider>
-                </Grid>
-            </Grid>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                {!isPhone
+                    ? <Grid container spacing={0}>
+                        <Grid item xs={2}>
+                            <ResponsiveDrawer></ResponsiveDrawer>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <Routes>
+                                <Route path="/" index element={<Home/>} />
+                                <Route path="/recommandation" element={<RecommendMoviesPage />} />
+                            </Routes>
+                        </Grid>
+                    </Grid>
+                    : <><ResponsiveDrawer></ResponsiveDrawer><Routes>
+                        <Route path="/" index element={<Home />} />
+                        <Route path="/recommandation" element={<RecommendMoviesPage />} />
+                    </Routes></>
+                }
+            </div>
+        </BrowserRouter>
     )
 }
 export default HomePage
