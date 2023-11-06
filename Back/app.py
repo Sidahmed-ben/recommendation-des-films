34c1b9a5bb6ef5d1439d7af00c4241431f938568
sidahmed.benaissa <benaissas886@gmail.com>
2023-11-06 23:45:51 +0100

    Revert "add docs dir"
    This reverts commit 96ba18d0cae3f7db977cacc4d053c71fdec8285c.

--- Back/app.py
+++ Back/app.py
@@ -8,5 +8,5 @@ def home():
     return '<h1>Hello from Flask & Docker</h2>'
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 6000))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
