4f44fd450c1d2efc50956e48a2d0216cd95b0e55
sidahmed.benaissa <benaissas886@gmail.com>
2023-11-18 14:47:04 +0100

    delete cred

--- omk.js
+++ omk.js
@@ -1,11 +1,3 @@
-// const pa = {
-//   mail: "benaissas886@gmail.com",
-//   apiOmk: "http://localhost:8080/omeka-s/api/",
-//   ident: "L9FGPCojmP9WpRcpG8ED0wd3wWa8dQq1",
-//   key: "fjqqrTQghi4CpugbdnovuPUir3p7pk6e",
-//   // omkKey: "	L9FGPCojmP9WpRcpG8ED0wd3wWa8dQq1",
-// };
-
 export class omk {
   constructor(params) {
     var me = this;
