5bf3c94769ee28d38063b7d5f18feed3107b3a12
sidahmed.benaissa <benaissas886@gmail.com>
2023-12-02 13:22:47 +0100

    Add getting users ratings + users recommandation + token verification

--- Back/controllers/dbController.py
+++ Back/controllers/dbController.py
@@ -1,3 +1,6 @@
+import re
+
+
 def indexMoviesDB(movieTable):
     movieTable.create_from_csv('./movies_dataset/movies.csv')
 
@@ -20,8 +23,101 @@ def createUserDB(db,user,username,email):
     db.session.commit()
 
 
-def getMoviesToRecommendDB(db,user,username,email):
-    # createUserDb(user)
-    newUser = user(username=username,email=email)
-    db.session.add(newUser)
+
+def getMoviesIdDB(movieTable):# Use the query method to retrieve the movie with the specified ID
+    # Requête pour récupérer tous les IDs des films
+    all_movie_ids = [movie.id for movie in movieTable.query.all()]
+    return all_movie_ids
+
+def getUserByEmail(userTable,email):# Use the query method to retrieve the movie with the specified ID
+    # Requête pour récupérer tous les IDs des films
+    user = userTable.query.filter_by(email=email).first()
+    return user
+
+def addUserRatingDB(db,movieRating,user_id_value,movie_id_value,rating_value):
+    # Check if the record already exists
+    existing_rating = movieRating.query.filter_by(user_id=user_id_value, movie_id=movie_id_value).first()
+
+    if existing_rating:
+        # If the record exists, update the existing row
+        existing_rating.rating = rating_value
+    else:
+    # If the record doesn't exist, create a new row
+        new_rating = movieRating(rating=rating_value, user_id=user_id_value, movie_id=movie_id_value)
+        db.session.add(new_rating)
+    # Commit the changes to the database
     db.session.commit()
+
+def getUserRatingsDB(db,movieTable,movieRatingTable,user_id_value):
+     # Query ratings for a specific user_id
+    user_ratings_with_titles = db.session.query(movieRatingTable.rating, movieTable.title).\
+    join(movieTable, movieRatingTable.movie_id == movieTable.id).\
+    filter(movieRatingTable.user_id == user_id_value).all()
+    ratings_list = []
+    for rating, title in user_ratings_with_titles:
+        rating_item = {}
+        rating_item["title"] = title
+        rating_item["rating"] = rating
+        match = re.search(r'\((\d{4})\)', rating_item['title'])
+        # Check if a match is found
+        if match:
+            year = match.group(1)
+            rating_item["year"] = year
+            rating_item["title"] = re.sub(r'\(\d{4}\)', '', rating_item["title"]).strip()
+            # return int(year)
+        else:
+            rating_item["year"] = 0
+        ratings_list.append(rating_item)
+    return ratings_list
+
+
+
+def concatenateUserRatings(db,movieRating,user_id,new_ratings):
+    # Requête pour obtenir les notes des films d'un utilisateur spécifique
+     # Requête pour obtenir les notes des films d'un utilisateur spécifique
+    user_ratings = db.session.query(movieRating.movie_id, movieRating.rating).\
+        filter(movieRating.user_id == user_id).all()
+    movies_ratings_list = [{"movieId": movie_id, "rating": rating} for movie_id, rating in user_ratings]
+    for movie_info in new_ratings:
+        movieId = movie_info["movieId"]
+        rating = movie_info["rating"]
+        is_movie_id_included = any(entry["movieId"] == movieId for entry in movies_ratings_list)
+        if(not is_movie_id_included):
+            movies_ratings_list.append({"movieId": movieId, "rating": rating})
+    return movies_ratings_list
+
+
+def addUserRecommendedDB(db,movieRecommanded,user_id_value,movie_id_value):
+        # Check if the record already exists
+    existing_rating = movieRecommanded.query.filter_by(user_id=user_id_value, movie_id=movie_id_value).first()
+    if existing_rating:
+        return
+    else:
+        # Create a new movieRating instance
+        new_rec = movieRecommanded(user_id=user_id_value, movie_id=movie_id_value)
+        # Add the new instance to the session
+        db.session.add(new_rec)
+        # Commit the changes to the database
+        db.session.commit()
+
+
+
+def getUserRecommendedMoviesDB(db,movieTable,movieRecommanded,user_id_value):
+    recommended_movies = db.session.query(movieTable.title).\
+    join(movieRecommanded, movieTable.id == movieRecommanded.movie_id).\
+    filter(movieRecommanded.user_id == user_id_value).all()
+    ratings_list = []
+    for (title,) in recommended_movies:
+        rating_item = {}
+        rating_item["title"] = title
+        match = re.search(r'\((\d{4})\)', rating_item['title'])
+        # Check if a match is found
+        if match:
+            year = match.group(1)
+            rating_item["year"] = year
+            rating_item["title"] = re.sub(r'\(\d{4}\)', '', rating_item["title"]).strip()
+            # return int(year)
+        else:
+            rating_item["year"] = 0
+        ratings_list.append(rating_item)
+    return ratings_list
---
+++ Back/controllers/learning.py
@@ -0,0 +1,80 @@
+import random
+import pandas as pd
+from scipy.sparse import csr_matrix
+from controllers.dbController import getMoviesIdDB
+
+def recommander_function(n,model,movies,ratings,userRatings):
+    
+    movies_users = ratings.pivot_table(index='userId',columns="movieId", values="rating", aggfunc='mean').fillna(0)
+
+    # # compressed_movies_users = csr_matrix(movies_users)
+    # print("##################")
+    # print("##################")
+    # # print("Modeel officiel shape =>> ",compressed_movies_users.shape)
+    # print("##################")
+    # print("##################")
+    
+    model_input =  get_model_input(movies_users,userRatings)
+    distance, indices  = model.kneighbors(model_input, n_neighbors=n)
+
+    # print(distance)
+    # print(indices)
+    indices = indices[0]
+    best_movies_ids = []
+    for i in indices :
+        similar_users = movies_users.iloc[i]
+        top_movies = similar_users[(similar_users == 5)]
+        # top_movies = similar_users[(similar_users == 5) | (similar_users == 4)]
+        best_movies_ids += top_movies.index.tolist()
+    best_movies_ids = list(set(best_movies_ids))
+    # print(best_movies_ids)
+    specific_movies_rows = movies[movies['movieId'].isin(best_movies_ids)]
+    # print(specific_movies_rows)
+
+    return specific_movies_rows 
+    # print(movies.loc[best_movies_ids])
+
+
+def get_model_input(movies_users,userRatings):
+    movie_ids_list = movies_users.columns.tolist()
+        
+    # Votre liste d'objets représentant les évaluations des utilisateurs
+    user_ratings_list = [
+        {
+            'userId': '999',
+             'moviesRatings': userRatings
+        },
+    ]
+
+    # Créer une DataFrame initiale avec tous les userId et movieId prédéfinis
+    user_ratings_df = pd.DataFrame([(user, movie) for user in ['999'] for movie in movie_ids_list], columns=['userId', 'movieId'])
+
+    # Fusionner avec la liste des notations pour remplir les notations existantes
+    for user_rating in user_ratings_list:
+        user_id = user_rating['userId']
+        movies_ratings = user_rating['moviesRatings']
+
+        for movie_rating in movies_ratings:
+            movie_id = movie_rating['movieId']
+            rating = movie_rating['rating']
+
+            # Mettre à jour la valeur de notation dans la DataFrame
+            user_ratings_df.loc[(user_ratings_df['userId'] == user_id) & (user_ratings_df['movieId'] == movie_id), 'rating'] = rating
+
+    # Remplir les valeurs manquantes avec 0
+    user_ratings_df['rating'].fillna(0, inplace=True)
+
+    # print(user_ratings_df)
+
+    # Utiliser pivot_table pour recréer la structure d'origine
+    movies_users_recreated = user_ratings_df.pivot_table(index='userId', columns='movieId', values='rating', aggfunc='mean').fillna(0)
+
+    # Convertir en une matrice creuse csr_matrix
+    compressed_movies_users_recreated = csr_matrix(movies_users_recreated)
+    # print("##################")
+    # print("##################")
+    # print("Mon  model shape =>> ",compressed_movies_users_recreated.shape)
+    # print("##################")
+    # print("##################")
+
+    return compressed_movies_users_recreated[0]
