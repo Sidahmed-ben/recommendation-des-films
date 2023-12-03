90282d7274f5c2e03b0e096aadad900202609056
sidahmed.benaissa <benaissas886@gmail.com>
2023-12-03 11:20:56 +0100

    Add some corrections

--- Front/movie-recommendation/src/components/Movie/MoviePagination.jsx
+++ Front/movie-recommendation/src/components/Movie/MoviePagination.jsx
@@ -1,67 +1,75 @@
-import React, { useState } from 'react'
-import { Box, Pagination } from '@mui/material'
-import MovieCard from './MovieCard'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import { Box, Pagination } from "@mui/material";
+import MovieCard from "./MovieCard";
+import PropTypes from "prop-types";
 
-export default function MoviesPagination ({ moviesToDisplay }) {
-    const itemsPerPage = 6
-    const [currentPage, setCurrentPage] = useState(1)
+export default function MoviesPagination({ moviesToDisplay }) {
+  const itemsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const indexOfLastMovie = currentPage * itemsPerPage
-    const indexOfFirstMovie = indexOfLastMovie - itemsPerPage
-    const currentMovies = moviesToDisplay.slice(indexOfFirstMovie, indexOfLastMovie)
+  const indexOfLastMovie = currentPage * itemsPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - itemsPerPage;
+  const currentMovies = moviesToDisplay.slice(
+    indexOfFirstMovie,
+    indexOfLastMovie
+  );
 
-    const changePage = (event, value) => {
-        setCurrentPage(value)
-    }
+  const changePage = (event, value) => {
+    setCurrentPage(value);
+  };
 
-    const containerStyle = {
-        width: '100%',
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center'
-    }
+  const containerStyle = {
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  };
 
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
-            }}
-        >
-            <Box
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    flexWrap: 'wrap',
-                    justifyContent: 'center'
-                }}
-            >
-                {currentMovies.map((movie, index) => (
-                    <Box key={index} sx={{ containerStyle }}>
-                        <MovieCard movie={movie} />
-                    </Box>
-                ))}
-            </Box>
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    marginTop: 2
-                }}
-            >
-                <Pagination
-                    count={Math.ceil(moviesToDisplay.length / itemsPerPage)}
-                    variant="outlined"
-                    page={currentPage}
-                    onChange={changePage}
-                />
-            </Box>
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {currentMovies.map((movie, index) => {
+          if (movie) {
+            return (
+              <Box key={index} sx={{ containerStyle }}>
+                <MovieCard movie={movie} />
+              </Box>
+            );
+          }
+          return null;
+        })}
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginTop: 2,
+        }}
+      >
+        <Pagination
+          count={Math.ceil(moviesToDisplay.length / itemsPerPage)}
+          variant="outlined"
+          page={currentPage}
+          onChange={changePage}
+        />
+      </Box>
+    </Box>
+  );
 }
 
 MoviesPagination.propTypes = {
-    moviesToDisplay: PropTypes.array.isRequired
-}
+  moviesToDisplay: PropTypes.array.isRequired,
+};
--- Front/movie-recommendation/src/components/Movie/Movies.jsx
+++ Front/movie-recommendation/src/components/Movie/Movies.jsx
@@ -1,60 +1,73 @@
-import React, { useContext, useEffect } from 'react'
-import GenresMovies from './Genres'
-import MovieContext from '../../MovieData'
-import { Typography } from '@mui/material'
-import MovieCard from './MovieCard'
-import InfiniteScroll from 'react-infinite-scroll-component'
-import { LoaderComponent } from '../Loader'
+import React, { useContext, useEffect } from "react";
+import GenresMovies from "./Genres";
+import MovieContext from "../../MovieData";
+import { Typography } from "@mui/material";
+import MovieCard from "./MovieCard";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { LoaderComponent } from "../Loader";
 
-function Movies () {
-    const {
-        movies, loader, page, setPage, totalPages, getMovies, selectedGenre, setSelectedGenre, getMoviesByGenres
-    } = useContext(MovieContext)
+function Movies() {
+  const {
+    movies,
+    loader,
+    page,
+    setPage,
+    totalPages,
+    getMovies,
+    selectedGenre,
+    setSelectedGenre,
+    getMoviesByGenres,
+  } = useContext(MovieContext);
 
-    const containerStyle = {
-        width: '100%',
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center'
+  const containerStyle = {
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  };
+
+  useEffect(() => {
+    setPage(1);
+    setSelectedGenre(0);
+  }, []);
+
+  useEffect(() => {
+    if (page > 0) {
+      selectedGenre === 0 ? getMovies() : getMoviesByGenres(selectedGenre);
     }
+  }, [page]);
 
-    useEffect(() => {
-        setPage(1)
-        setSelectedGenre(0)
-    }, [])
-
-    useEffect(() => {
-        if (page > 0) {
-            selectedGenre === 0 ? getMovies() : getMoviesByGenres(selectedGenre)
-        }
-    }, [page])
-
-    return (
-        <div>
-            <Typography align='center' variant='h3'>Films</Typography>
-            {
-                !loader
-                    ? <>
-                        <GenresMovies></GenresMovies>
-                        <InfiniteScroll
-                            style={containerStyle}
-                            dataLength={movies.length}
-                            next={() => {
-                                setPage(page + 1)
-                            }}
-                            hasMore={page < totalPages}
-                            loader={<LoaderComponent />}
-                            scrollThreshold={0.9}
-                        >
-                            {movies.map((movie, index) => (
-                                <MovieCard key={index} movie={movie} />
-                            ))}
-                        </InfiniteScroll>
-                    </>
-                    : <LoaderComponent />
-            }
-        </div>
-    )
+  return (
+    <div>
+      <Typography align="center" variant="h3">
+        Films
+      </Typography>
+      {!loader ? (
+        <>
+          <GenresMovies></GenresMovies>
+          <InfiniteScroll
+            style={containerStyle}
+            dataLength={movies.length}
+            next={() => {
+              setPage(page + 1);
+            }}
+            hasMore={page < totalPages}
+            loader={<LoaderComponent />}
+            scrollThreshold={0.9}
+          >
+            {movies.map((movie, index) => {
+              if (movie) {
+                return <MovieCard key={index} movie={movie} />;
+              }
+              return null;
+            })}
+          </InfiniteScroll>
+        </>
+      ) : (
+        <LoaderComponent />
+      )}
+    </div>
+  );
 }
 
-export default Movies
+export default Movies;
